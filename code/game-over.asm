ddd85afda7b00e21f8e12fa9fe2e32e44334f896
martendo <51766598+martendo@users.noreply.github.com>
2021-05-12 17:42:18 -0400

    Save a new top score on game over
    Perhaps a player might get mad and turn off the console before moving on
    to the next screen (top scores screen), so update the top scores right
    when the game is over.

--- code/game-over.asm
+++ code/game-over.asm
@@ -40,6 +40,95 @@ LoadGameOverScreen::
     ld      [hl], a     ; 2 cycles
     ; Total 15 cycles
 .noPowerUps
+    
+    ; Check if this score is a new high score
+    ld      a, CART_SRAM_ENABLE
+    ld      [rRAMG], a
+    
+    ; Get corresponding top scores list to check based on game mode
+    ld      de, sClassicTopScores
+    ldh     a, [hGameMode]
+    ASSERT GAME_MODE_CLASSIC == 0
+    and     a, a
+    jr      z, :+
+    ASSERT GAME_MODE_COUNT - 1 == 1
+    ASSERT sSuperTopScores == sClassicTopScores + (1 << 8)
+    inc     d
+:
+    ld      hl, hScore
+    lb      bc, SCORE_BYTE_COUNT, 0
+.checkHighScoreLoop
+    ld      a, [de]
+    cp      a, [hl]
+    jr      c, .insertTopScore  ; Top score < Score
+    jr      nz, .nextTopScore   ; Top score > Score
+    dec     b
+    jr      z, .insertTopScore  ; Top score == Score
+    inc     e
+    inc     l
+    jr      .checkHighScoreLoop
+    
+.nextTopScore
+    inc     c
+    ld      a, c
+    cp      a, TOP_SCORE_COUNT
+    ; Not a new top score
+    jr      nc, .notNewTopScore
+    
+    ASSERT SCORE_BYTE_COUNT == 3
+    add     a, a
+    add     a, c
+    ld      e, a
+    ld      l, LOW(hScore)
+    ld      b, SCORE_BYTE_COUNT
+    jr      .checkHighScoreLoop
+    
+.insertTopScore
+    ASSERT LOW(sClassicTopScores.end - 1 - SCORE_BYTE_COUNT) == LOW(sSuperTopScores.end - 1 - SCORE_BYTE_COUNT)
+    ld      e, LOW(sClassicTopScores.end - 1 - SCORE_BYTE_COUNT)
+    ld      h, d
+    ASSERT LOW(sClassicTopScores.end - 1) == LOW(sSuperTopScores.end - 1)
+    ld      l, LOW(sClassicTopScores.end - 1)
+    
+    ld      a, TOP_SCORE_COUNT - 1
+    sub     a, c
+    jr      z, .saveScore   ; No shifting to do
+    ld      b, a
+.shiftTopScoresLoop
+    REPT SCORE_BYTE_COUNT
+    ld      a, [de]
+    ld      [hld], a
+    dec     e
+    ENDR
+    dec     b
+    jr      nz, .shiftTopScoresLoop
+.saveScore
+    ld      a, c
+    ASSERT SCORE_BYTE_COUNT == 3
+    add     a, a
+    add     a, c
+    ld      e, a
+    
+    ld      hl, hScore
+    REPT SCORE_BYTE_COUNT - 1
+    ld      a, [hli]
+    ld      [de], a
+    inc     e
+    ENDR
+    ld      a, [hl]
+    ld      [de], a
+    
+    ; Save the index of the new top score for the top scores screen
+    ld      a, c
+    DB      $01     ; ld bc, d16 to consume the next 2 bytes
+.notNewTopScore
+    ld      a, NO_NEW_TOP_SCORE
+    ldh     [hScratch], a
+    
+    ASSERT CART_SRAM_DISABLE == 0
+    xor     a, a
+    ld      [rRAMG], a
+    
     jp      Music_Pause
 
 GameOver::
--- code/top-scores.asm
+++ code/top-scores.asm
@@ -12,92 +12,20 @@ LoadTopScoresScreen::
     ld      c, SCRN_Y_B
     call    LCDMemcopyMap
     
-    ; Check if this score is a new high score
-    ld      a, CART_SRAM_ENABLE
-    ld      [rRAMG], a
+    ; Check if there's a new top score
+    ldh     a, [hScratch]   ; Index of new top score
+    ASSERT NO_NEW_TOP_SCORE + 1 == 0
+    inc     a
+    jr      z, .drawTopScores
     
-    ; Get corresponding top scores list to check based on game mode
-    ld      de, sClassicTopScores
-    ldh     a, [hGameMode]
-    ASSERT GAME_MODE_CLASSIC == 0
-    and     a, a
-    jr      z, :+
-    ASSERT GAME_MODE_COUNT - 1 == 1
-    ASSERT sSuperTopScores == sClassicTopScores + (1 << 8)
-    inc     d
-:
-    push    de      ; Save to draw it onscreen later
-    ld      hl, hScore
-    lb      bc, SCORE_BYTE_COUNT, 0
-.checkHighScoreLoop
-    ld      a, [de]
-    cp      a, [hl]
-    jr      c, .insertTopScore  ; Top score < Score
-    jr      nz, .nextTopScore   ; Top score > Score
-    dec     b
-    jr      z, .insertTopScore  ; Top score == Score
-    inc     e
-    inc     l
-    jr      .checkHighScoreLoop
-    
-.nextTopScore
-    inc     c
-    ld      a, c
-    cp      a, TOP_SCORE_COUNT
-    ; Not a new top score
-    jr      nc, .drawTopScores
-    
-    ASSERT SCORE_BYTE_COUNT == 3
-    add     a, a
-    add     a, c
-    ld      e, a
-    ld      l, LOW(hScore)
-    ld      b, SCORE_BYTE_COUNT
-    jr      .checkHighScoreLoop
-    
-.insertTopScore
-    ASSERT LOW(sClassicTopScores.end - 1 - SCORE_BYTE_COUNT) == LOW(sSuperTopScores.end - 1 - SCORE_BYTE_COUNT)
-    ld      e, LOW(sClassicTopScores.end - 1 - SCORE_BYTE_COUNT)
-    ld      h, d
-    ASSERT LOW(sClassicTopScores.end - 1) == LOW(sSuperTopScores.end - 1)
-    ld      l, LOW(sClassicTopScores.end - 1)
-    
-    ld      a, TOP_SCORE_COUNT - 1
-    sub     a, c
-    jr      z, .saveScore   ; No shifting to do
-    ld      b, a
-.shiftTopScoresLoop
-    REPT SCORE_BYTE_COUNT
-    ld      a, [de]
-    ld      [hld], a
-    dec     e
-    ENDR
-    dec     b
-    jr      nz, .shiftTopScoresLoop
-.saveScore
-    ld      a, c
-    ASSERT SCORE_BYTE_COUNT == 3
-    add     a, a
-    add     a, c
-    ld      e, a
-    
-    ld      hl, hScore
-    REPT SCORE_BYTE_COUNT - 1
-    ld      a, [hli]
-    ld      [de], a
-    inc     e
-    ENDR
-    ld      a, [hl]
-    ld      [de], a
-    
+    dec     a       ; Undo inc
     ; Show "NEW" sprite
     ld      hl, wShadowOAM
     ; Object 1
-    ld      a, c
-    add     a, a
-    add     a, a
-    add     a, a
-    add     a, a
+    add     a, a    ; * 2
+    add     a, a    ; * 4
+    add     a, a    ; * 8
+    add     a, a    ; * 16
     add     a, NEW_TOP_SCORE_START_Y
     ld      [hli], a
     ld      [hl], NEW_TOP_SCORE_X
@@ -116,7 +44,19 @@ LoadTopScoresScreen::
     
 .drawTopScores
     ; Draw high score
-    pop     de
+    ld      a, CART_SRAM_ENABLE
+    ld      [rRAMG], a
+    
+    ; Get corresponding top scores list to draw based on game mode
+    ld      de, sClassicTopScores
+    ldh     a, [hGameMode]
+    ASSERT GAME_MODE_CLASSIC == 0
+    and     a, a
+    jr      z, :+
+    ASSERT GAME_MODE_COUNT - 1 == 1
+    ASSERT sSuperTopScores == sClassicTopScores + (1 << 8)
+    inc     d
+:
     DEF OFFSET = 0
     ld      hl, vGameOverTopScores
     lb      bc, TOP_SCORES_NUMBER_TILES_START, SCORE_BYTE_COUNT
