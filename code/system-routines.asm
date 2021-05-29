067f732400b6cb654dfe3497c395bd699d287eeb
martendo <51766598+martendo@users.noreply.github.com>
2021-05-29 17:06:32 -0400

    Remove unused Memset routine

--- code/system-routines.asm
+++ code/system-routines.asm
@@ -70,20 +70,6 @@ LCDMemcopyMap::
     jr      nz, LCDMemcopyMap
     ret
 
-; Set a block of memory to a single byte value
-; @param    hl  Pointer to destination
-; @param    a   Byte value to use
-; @param    bc  Number of bytes to set
-Memset::
-    ld      d, a
-.loop
-    ld      a, d
-    ld      [hli], a
-    dec     bc
-    ld      a, c
-    or      a, b
-    jr      nz, .loop
-    ret
 ; @param    hl  Pointer to destination
 ; @param    a   Byte value to use
 ; @param    b   Number of bytes to set
